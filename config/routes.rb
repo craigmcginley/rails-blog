83dbfd3185a6e4c4cf97aff443ed5a4dc005e410
craigmcginley <craigmcginley@gmail.com>
2014-06-13 16:10:55 -0400

    uhhh

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :articles, except: [:edit, :update, :destroy] do
+    resources :comments, only: :create
+  end
+
 end
